16be87d7cd42982975564c1a8e9787e10ce3bdf5
Subro sutradhar <subroopu@gmail.com>
2026-03-05 02:39:58 +0600

    Create Eligibility Checker.py

---
+++ Eligibility Checker.py
@@ -0,0 +1,16 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are NOT eligible to vote")
+
+if age >= 16:
+    print("You are eligible to drive")
+else:
+    print("You are NOT eligible to drive")
+
+if age >= 60:
+    print("You are eligible for retirement")
+else:
+    print("You are NOT eligible for retirement")
